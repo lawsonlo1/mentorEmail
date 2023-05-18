32104a2321311a10d65b919f21bb5e9db2130bc1
Logan Lawson <130174775+lawsonlo1@users.noreply.github.com>
2023-05-18 11:39:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def landing_page():
-    if request.method == 'POST':
-        email = request.form['email']
-        with open('emails.txt', 'a') as file:
-            file.write(email + '\n')
-        return 'Thank you for submitting your email!'
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5988)
+    app.run(port=5000)
